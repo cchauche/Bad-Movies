f4f91e1c0cb8735fc41d73f032b0e0ba77b4c3b2
Colin Chauche <colin.chauche@gmail.com>
2020-12-28 12:12:47 -0500

    Stub out apiHelpers

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -3,6 +3,17 @@ const axios = require('axios');
 const { API_KEY } = require('../../config.js');
 
 // write out logic/functions required to query TheMovieDB.org
+const discoverURL = 'https://api.themoviedb.org/3/discover/movie';
+const genreURL = 'https://api.themoviedb.org/3/genre/movie/list';
+
+const getTMDBGenres = () => {
+  // Get  list of genres from API
+  // Return a list of genres with their ids?
+}
+
+const getWorstOfGenre = (genreId) => {
+  //Search for worst rated movies of a given genre
+}
 
 // FOR REFERENCE:
 // https://www.themoviedb.org/account/signup
