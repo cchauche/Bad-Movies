ef9a41a0851f5bc221a49829e224adbaf010b553
Colin Chauche <colin.chauche@gmail.com>
2020-12-28 11:51:17 -0500

    Add basic schema for badmovies DB

--- db/sql/moviesSchema.sql
+++ db/sql/moviesSchema.sql
@@ -1,1 +1,14 @@
 -- SET UP SCHEMA HERE
+DROP DATABASE IF EXISTS badmovies;
+CREATE DATABASE badmovies;
+USE badmovies;
+
+DROP TABLE IF EXISTS favorites;
+CREATE TABLE favorites (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(50),
+  year INT,
+  rating DECIMAL(3,1),
+  image LONGBLOB
+);
