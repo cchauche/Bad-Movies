9d18df8ee6f8f52a54a52969145ebbfe1e15aed2
Colin Chauche <colin.chauche@gmail.com>
2020-12-28 16:32:10 -0500

    Implement apiHelpers and controllers for /movies/searches route

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,37 +1,45 @@
-const movieModel = require('../models/movieModel.js');
-const apiHelpers = require('../helpers/apiHelpers.js');
+const movieModel = require("../models/movieModel.js");
+const apiHelpers = require("../helpers/apiHelpers.js");
 
 //Return requests to the client
 module.exports = {
   getSearch: (req, res) => {
     // get the search genre
-
     // https://www.themoviedb.org/account/signup
     // get your API KEY
-
     // use this endpoint to search for movies by genres, you will need an API key
-
     // https://api.themoviedb.org/3/discover/movie
-
     // and sort them by horrible votes using the search parameters in the API
+    if (!req.query.hasOwnProperty("genre_id")) {
+      res.sendStatus(406);
+    } else {
+      apiHelpers
+        .getWorstOfGenre(req.query.genre_id)
+        .then((responseData) => {
+          res.status(200).json(responseData.results);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.sendStatus(501);
+        });
+    }
   },
   getGenres: (req, res) => {
     // make an axios request to get the list of official genres
-    apiHelpers.getTMDBGenres().then((genres) => {
-      res.status(200).json(genres);
-    }).catch((err) => {
-      console.error(err)
-      res.sendStatus(501);
-    })
+    apiHelpers
+      .getTMDBGenres()
+      .then((genres) => {
+        res.status(200).json(genres);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(501);
+      });
 
     // use this endpoint, which will also require your API key: https://api.themoviedb.org/3/genre/movie/list
 
     // send back
   },
-  saveMovie: (req, res) => {
-
-  },
-  deleteMovie: (req, res) => {
-
-  }
-}
+  saveMovie: (req, res) => {},
+  deleteMovie: (req, res) => {},
+};
--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -1,21 +1,31 @@
-const request = require('request');
-const axios = require('axios').default;
-const { API_KEY } = require('../../config.js');
+const request = require("request");
+const axios = require("axios").default;
+const { API_KEY } = require("../../config.js");
 
 // write out logic/functions required to query TheMovieDB.org
-const discoverURL = 'https://api.themoviedb.org/3/discover/movie' + '?api_key=' + API_KEY;
-const genreURL = 'https://api.themoviedb.org/3/genre/movie/list' + '?api_key=' + API_KEY;
+const discoverURL =
+  "https://api.themoviedb.org/3/discover/movie" + "?api_key=" + API_KEY;
+const genreURL =
+  "https://api.themoviedb.org/3/genre/movie/list" + "?api_key=" + API_KEY;
 
 const getTMDBGenres = () => {
   // Get  list of genres from API
   return axios.get(genreURL).then((response) => {
     return response.data.genres;
-  })
-}
+  });
+};
 
 const getWorstOfGenre = (genreId) => {
   //Search for worst rated movies of a given genre
-}
+  let params = {
+    language: "en-US",
+    with_genres: genreId,
+    sort_by: "vote_average.asc",
+  };
+  return axios.get(discoverURL, { params }).then((response)=>{
+    return response.data;
+  });
+};
 
 // FOR REFERENCE:
 // https://www.themoviedb.org/account/signup
@@ -24,5 +34,6 @@ const getWorstOfGenre = (genreId) => {
 
 // Don't forget to export your functions and require them within your server file
 module.exports = {
-  getTMDBGenres, getWorstOfGenre
-}
+  getTMDBGenres,
+  getWorstOfGenre,
+};
