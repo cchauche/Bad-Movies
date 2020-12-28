42acbd1ebfbeb7de5b57b60da7f1bc293e07f35e
Colin Chauche <colin.chauche@gmail.com>
2020-12-28 18:50:57 -0500

    Add componentDidMount function to get genres from api call

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,124 +1,80 @@
-import React from 'react';
-
-let dummyGenres = [
-  {
-      "id": 28,
-      "name": "Action"
-  },
-  {
-      "id": 12,
-      "name": "Adventure"
-  },
-  {
-      "id": 16,
-      "name": "Animation"
-  },
-  {
-      "id": 35,
-      "name": "Comedy"
-  },
-  {
-      "id": 80,
-      "name": "Crime"
-  },
-  {
-      "id": 99,
-      "name": "Documentary"
-  },
-  {
-      "id": 18,
-      "name": "Drama"
-  },
-  {
-      "id": 10751,
-      "name": "Family"
-  },
-  {
-      "id": 14,
-      "name": "Fantasy"
-  },
-  {
-      "id": 36,
-      "name": "History"
-  },
-  {
-      "id": 27,
-      "name": "Horror"
-  },
-  {
-      "id": 10402,
-      "name": "Music"
-  },
-  {
-      "id": 9648,
-      "name": "Mystery"
-  },
-  {
-      "id": 10749,
-      "name": "Romance"
-  },
-  {
-      "id": 878,
-      "name": "Science Fiction"
-  },
-  {
-      "id": 10770,
-      "name": "TV Movie"
-  },
-  {
-      "id": 53,
-      "name": "Thriller"
-  },
-  {
-      "id": 10752,
-      "name": "War"
-  },
-  {
-      "id": 37,
-      "name": "Western"
-  }
-];
+import React from "react";
+import axios from "axios";
 
 class Search extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      //todo: How do we get values in here to start out with?
-      genres: dummyGenres,
-      selectValue: ''
+      genres: [],
+      selectValue: "",
     };
+    this.getGenres = this.getGenres.bind(this);
   }
 
-  //todo: do we need an 'oncomponent load function to get genres and set selectValue?
+  componentDidMount() {
+    this.getGenres()
+      .then((genres) => {
+        this.setState({
+          genres: genres,
+          selectValue: genres[0].id
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 
   getGenres() {
-    //todo: make an axios request in this component to get the list of genres from your endpoint GET GENRES
+    return axios.get("http://localhost:3000/movies/genres").then((response) => {
+      return response.data;
+    });
   }
 
   handleChange(event) {
     // todo: remove this console log when done with component
-    console.log('New selection: ', event.target.value);
+    console.log("New selection: ", event.target.value);
     this.setState({
-      selectValue: event.target.value
-    })
+      selectValue: event.target.value,
+    });
   }
 
   render() {
     return (
       <div className="search">
-        <button onClick={() => {this.props.swapFavorites()}}>{this.props.showFaves ? "Show Results" : "Show Favorites"}</button>
-        <br/><br/>
+        <button
+          onClick={() => {
+            this.props.swapFavorites();
+          }}
+        >
+          {this.props.showFaves ? "Show Results" : "Show Favorites"}
+        </button>
+        <br />
+        <br />
 
-
-        <select value={this.state.selectValue} onChange={(e)=>{this.handleChange(e)}}>
+        <select
+          value={this.state.selectValue}
+          onChange={(e) => {
+            this.handleChange(e);
+          }}
+        >
           {this.state.genres.map((genre) => {
-            return (<option value={genre.id} key={genre.id}>{genre.name}</option>)
+            return (
+              <option value={genre.id} key={genre.id}>
+                {genre.name}
+              </option>
+            );
           })}
         </select>
-        <br/><br/>
+        <br />
+        <br />
 
-        <button onClick={() => {this.props.getMovies(this.state.selectValue)}}>Search</button>
-
+        <button
+          onClick={() => {
+            this.props.getMovies(this.state.selectValue);
+          }}
+        >
+          Search
+        </button>
       </div>
     );
   }
