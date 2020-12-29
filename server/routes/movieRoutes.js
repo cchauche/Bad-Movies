b615ae692cf6319a4df97cd9e8485b60d83c5e49
Colin Chauche <colin.chauche@gmail.com>
2020-12-29 11:54:58 -0500

    Add a get route for '/movies/' which will return all movies in favorites table

--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -4,9 +4,12 @@ const router = require('express').Router();
 const movieController = require('../controllers/movieController.js');
 
 //Route different requests to different endpoints
+router.get('/', movieController.getFavorites)
 router.get('/search', movieController.getSearch)
 router.get('/genres', movieController.getGenres)
 router.post('/save', movieController.saveMovie)
 router.delete('/delete/:id', movieController.deleteMovie)
+
+
 
 module.exports = router;
