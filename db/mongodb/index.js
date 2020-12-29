759aab1258bb8ca9d413f51e06704e9cd90b8303
Colin Chauche <colin.chauche@gmail.com>
2020-12-29 16:01:20 -0500

    Stubbed out switch to mondoDB

--- db/mongodb/index.js
+++ db/mongodb/index.js
@@ -1,4 +1,4 @@
-// 
+//
 
 const mongoose = require('mongoose');
 if(process.env.MONGODB_URI){
--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,47 +1,75 @@
 //Select one db to work with:
 
 //For SQL
-const sqlDb = require("../../db/sql");
+// const sqlDb = require("../../db/sql");
 //For Mongo
-// const mongoDb = require('../../db/mongodb')
+const mongoDb = require('../../db/mongodb')
 
+
+/**
+ * *******************************************
+ * mySQL MODELS
+ * *******************************************
+ */
+// module.exports = {
+//   addFavorite: (
+//     { id, title, poster_path, release_date, vote_average },
+//     callback
+//   ) => {
+//     sqlQuery =
+//       "INSERT INTO favorites (movie_id, title, poster_path, release_date, vote_average) VALUES (?,?,?,?,?)";
+//     sqlValues = [id, title, poster_path, release_date, vote_average];
+//     sqlDb.query(sqlQuery, sqlValues, (err) => {
+//       if (err) {
+//         callback(err);
+//         return;
+//       }
+//       callback(null);
+//     });
+//   },
+
+//   deleteFavorite: (movieId, callback) => {
+//     sqlQuery = "DELETE FROM favorites WHERE movie_id=?";
+//     sqlValues = [movieId];
+//     sqlDb.query(sqlQuery, sqlValues, (err) => {
+//       if (err) {
+//         callback(err);
+//         return;
+//       }
+//       callback(null);
+//     });
+//   },
+
+//   getFavorites: (callback) => {
+//     sqlQuery = "SELECT movie_id AS id, title, release_date, vote_average, poster_path FROM favorites";
+//     sqlDb.query(sqlQuery, (err, results) => {
+//       if (err) {
+//         callback(err);
+//         return;
+//       }
+//       callback(null, results);
+//     });
+//   },
+// };
+
+/**
+ * *******************************************
+ * MongoDB MODELS
+ * *******************************************
+ */
 module.exports = {
   addFavorite: (
     { id, title, poster_path, release_date, vote_average },
     callback
   ) => {
-    sqlQuery =
-      "INSERT INTO favorites (movie_id, title, poster_path, release_date, vote_average) VALUES (?,?,?,?,?)";
-    sqlValues = [id, title, poster_path, release_date, vote_average];
-    sqlDb.query(sqlQuery, sqlValues, (err) => {
-      if (err) {
-        callback(err);
-        return;
-      }
-      callback(null);
-    });
+    //Add new movie to favorites
   },
 
   deleteFavorite: (movieId, callback) => {
-    sqlQuery = "DELETE FROM favorites WHERE movie_id=?";
-    sqlValues = [movieId];
-    sqlDb.query(sqlQuery, sqlValues, (err) => {
-      if (err) {
-        callback(err);
-        return;
-      }
-      callback(null);
-    });
+    // Delete a movie from favorites
   },
 
   getFavorites: (callback) => {
-    sqlQuery = "SELECT movie_id AS id, title, release_date, vote_average, poster_path FROM favorites";
-    sqlDb.query(sqlQuery, (err, results) => {
-      if (err) {
-        callback(err);
-        return;
-      }
-      callback(null, results);
-    });
+    // Get all the favorites
   },
 };
